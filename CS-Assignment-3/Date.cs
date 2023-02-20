b716155cf0b373fe0286c9631a86dd3619f50814
Jahnavi B R <jahnavi.b-r@capgemini.com>
2023-02-20 22:10:48 +0530

    Add project files.

---
+++ CS-Assignment-3/Date.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS_Assignment_3
+{
+    class Date
+    {
+        private int day;
+        private int month;
+        private int year;
+
+        public Date(int day, int month, int year)
+        {
+            this.day = day;
+            this.month = month;
+            this.year = year;
+        }
+
+        //Properties for the Date class Members
+
+        public int Day
+        {
+            get { return day; }
+            set { day = value; }
+        }
+        public int Month
+        {
+            get { return month; }
+            set { month = value; }
+        }
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+        public override string ToString()
+        {
+            return "" + Day + "/" + Month + "/" + Year;
+        }
+    }
+}
